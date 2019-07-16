287264293e6c849e4ae8ead8995e277b45488568
Alex Corn <mckay@doctormckay.com>
2019-07-16 04:17:26 -0400

    Modernized file-manager and swiched to Promise-based API

--- index.js
+++ index.js
@@ -1,145 +1,159 @@
-var Async = require('async');
-var fs = require('fs');
+const {EventEmitter} = require('events');
+const FS = require('fs');
+const Util = require('util');
 
-require('util').inherits(FileStorage, require('events').EventEmitter);
-module.exports = FileStorage;
+Util.inherits(FileManager, EventEmitter);
+module.exports = FileManager;
 
 /**
- * Creates a new FileStorage object
+ * Creates a new FileManager object
  * @param {string} directory - The local directory where files will be saved if events aren't listened to. No trailing slash. No nesting.
  * @constructor
  */
-function FileStorage(directory) {
+function FileManager(directory) {
 	Object.defineProperty(this, 'directory', {
 		"get": function() {
 			return this._directory;
 		},
 		"set": function(newDir) {
 			this._directory = newDir;
-			this._directoryCreated = false;
 		}
 	});
 
 	this._directory = null;
-	this._directoryCreated = null;
 	this.directory = directory;
 }
 
 /**
- * Checks whether or not the FileStorage object can store and retrieve files.
+ * Checks whether or not the FileManager object can store and retrieve files.
  * @returns bool
  */
-FileStorage.prototype.isEnabled = function() {
+FileManager.prototype.isEnabled = function() {
 	return (this.listeners('save').length > 0 && this.listeners('read').length > 0) || this.directory !== null;
 };
 
 /**
  * Saves a file
  * @param {string} filename - The name of the file
- * @param {Buffer|string} contents - The contents of the file
- * @param {function} [callback] - Called when the file is saved or an error occurs
+ * @param {Buffer|*} contents - The contents of the file
+ * @return {Promise}
  */
-FileStorage.prototype.saveFile = FileStorage.prototype.writeFile = function(filename, contents, callback) {
-	if(!this.isEnabled()) {
-		if(callback) {
-			callback(new Error("File storage system is not enabled"));
+FileManager.prototype.saveFile = FileManager.prototype.writeFile = function(filename, contents) {
+	return new Promise((resolve, reject) => {
+		if (!this.isEnabled()) {
+			return reject(new Error("File storage system is not enabled"));
 		}
-		
-		return;
-	}
 
-	if(typeof contents === 'string') {
-		contents = new Buffer(contents, 'utf8');
-	}
+		if (!Buffer.isBuffer(contents)) {
+			contents = Buffer.from(contents.toString(), 'utf8');
+		}
 
-	if(this.listeners('save').length > 0) {
-		this.emit('save', filename, contents, function(err) {
-			if(callback) {
-				callback(err || null);
+		if (this.listeners('save').length > 0) {
+			this.emit('save', filename, contents, (err) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve();
+				}
+			});
+
+			return;
+		}
+
+		checkDirExists(this.directory);
+
+		FS.writeFile(this.directory + '/' + filename, contents, (err) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve();
 			}
 		});
-
-		return;
-	}
-
-	checkDirExists(this.directory);
-	this._directoryCreated = true;
-
-	fs.writeFile(this.directory + '/' + filename, contents, callback || noop);
+	});
 };
 
 /**
  * Saves many files
  * @param {Object} files - Keys are filenames, values are Buffer objects containing the file contents
- * @param {function} [callback] - Called when all files are saved, or an error occurs
+ * @return {Promise}
  */
-FileStorage.prototype.saveFiles = FileStorage.prototype.writeFiles = function(files, callback) {
-	var self = this;
-	Async.each(Object.keys(files), function(filename, cb) {
-		self.saveFile(filename, files[filename], cb);
-	}, callback || noop);
+FileManager.prototype.saveFiles = FileManager.prototype.writeFiles = function(files) {
+	return new Promise(async (resolve, reject) => {
+		try {
+			await Promise.all(Object.keys(files).map(filename => this.saveFile(filename, files[filename])));
+			resolve();
+		} catch (ex) {
+			reject(ex);
+		}
+	});
 };
 
 /**
  * Reads the contents of a single file
  * @param {string} filename - The name of the file to read
- * @param {function} callback - Called when read, first argument is an Error object or null, second is a Buffer object
+ * @return {Promise}
  */
-FileStorage.prototype.readFile = function(filename, callback) {
-	if(!this.isEnabled()) {
-		callback(new Error("File storage system is not enabled"));
-		return;
-	}
+FileManager.prototype.readFile = function(filename) {
+	return new Promise((resolve, reject) => {
+		if (!this.isEnabled()) {
+			return reject(new Error("File storage system is not enabled"));
+		}
 
-	if(this.listeners('read').length > 0) {
-		this.emit('read', filename, callback);
-		return;
-	}
+		if (this.listeners('read').length > 0) {
+			this.emit('read', filename, (err, content) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(content);
+				}
+			});
 
-	fs.readFile(this.directory + '/' + filename, callback);
+			return;
+		}
+
+		FS.readFile(this.directory + '/' + filename, (err, content) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(content);
+			}
+		});
+	});
 };
 
 /**
  * Reads the contents of multiple files
  * @param {string[]} filenames - An array of filenames
- * @param {function} callback - Called when read. Array with same order as input array, each element is an object with filename and contents properties.
+ * @return {Promise<Array>} - Array with same order as input array, each element is an object with filename and contents properties.
  */
-FileStorage.prototype.readFiles = function(filenames, callback) {
-	var self = this;
-	Async.map(filenames, function(filename, cb) {
-		self.readFile(filename, function(err, file) {
-			var response = {"filename": filename};
-
-			if(err) {
-				response.error = err;
-			} else {
-				response.contents = file;
+FileManager.prototype.readFiles = function(filenames) {
+	return Promise.all(filenames.map((filename) => {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let contents = await this.readFile(filename);
+				return resolve({filename, contents});
+			} catch (error) {
+				resolve({filename, error});
 			}
-
-			cb(null, response);
 		});
-	}, callback);
+	}));
 };
 
 function checkDirExists(dir) {
-	if(!dir) {
+	if (!dir) {
 		return;
 	}
 
-	var path = '';
+	let path = '';
 	dir.replace(/\\/g, '/').split('/').forEach(function(dir, index) {
-		if(index === 0 && !dir) {
+		if (index === 0 && !dir) {
 			path = '/';
 		} else {
 			path += (path ? '/' : '') + dir;
 		}
 
-		if(!fs.existsSync(path)) {
-			fs.mkdirSync(path, 0o750);
+		if (!FS.existsSync(path)) {
+			FS.mkdirSync(path, 0o750);
 		}
 	});
-}
-
-function noop() {
-	// hello
 }
